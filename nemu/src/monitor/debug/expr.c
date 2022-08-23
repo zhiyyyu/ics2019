d9c8e75283480cadc4d45142280fd6f49da58701
tracer-ics2019 <tracer@njuics.org>
2022-08-24 04:11:59 +0800

    >  compile 201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  04:11:59 up  5:46,  0 users,  load average: 0.20, 0.11, 0.07 8692458f7a8c4887b05208469aa515fcb6423b80

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -218,7 +218,7 @@ bool check_parentheses(uint32_t p, uint32_t q){
 uint32_t get_main_op(uint32_t p, uint32_t q){
   int p_idx = 0, t_idx = -1;
   for(int i=q;i>=(int)p;i--){
-    // Log("t_idx %d i %d\n", t_idx, i);
+    Log("t_idx %d i %d\n", t_idx, i);
     if(tokens[i].type == ')'){
       p_idx++;
     } else if(tokens[i].type == '('){
