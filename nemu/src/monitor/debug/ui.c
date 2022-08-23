7f28b59c91a73381790844bbc0f1263ce552d3d0
tracer-ics2019 <tracer@njuics.org>
2022-08-23 12:23:40 +0800

    >  compile 201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:23:40 up  1:06,  0 users,  load average: 0.00, 0.02, 0.00 f358febadf4c7718aa2f85ef462a9841e95bc51

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -51,7 +51,7 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-  { "si", " [N]: Execute N step", cmd_si },
+  { "si", "si [N]: Execute N step", cmd_si },
 
   /* TODO: Add more commands */
 
