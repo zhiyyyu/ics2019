1fbc72e999b57dc12956ec5ccef93c1a94032b28
tracer-ics2019 <tracer@njuics.org>
2022-08-24 02:27:38 +0800

    >  compile 201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:27:38 up  4:02,  0 users,  load average: 0.18, 0.12, 0.06 3f33c4922908ce22813d062ee415fd9d7d4a029f

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -11,6 +11,7 @@ enum {
   TK_EQ, TK_NEQ,
   TK_REG,
   TK_NUM, TK_HEX,
+  TK_DEREF,
   /* TODO: Add more token types */
 
 };
@@ -66,11 +67,13 @@ typedef struct token {
 
 static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
+extern const char* regsl[];
 
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
+  char reg_name[32];
 
   nr_token = 0;
 
@@ -80,6 +83,7 @@ static bool make_token(char *e) {
       // 匹配正则表达式
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
         char *substr_start = e + position;
+        // eo: end, so: start
         int substr_len = pmatch.rm_eo;
 
         Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
@@ -91,10 +95,24 @@ static bool make_token(char *e) {
          * of tokens, some extra actions should be performed.
          */
         
-        // switch (rules[i].token_type) {
-        
-        // default: TODO();
-        // }
+        switch (rules[i].token_type) {
+        case TK_NUM: case TK_HEX:
+          strncpy(tokens[nr_token].str, substr_start, substr_len);
+          break;
+        case TK_REG:
+          strncpy(reg_name, substr_start, substr_len);
+          if(!strcmp(reg_name, "$pc")){
+            strcpy(tokens[nr_token].str, itoa(cpu.pc)); break;
+          }
+          for(int i=0;i<32;i++){
+            if(!strcmp(reg_name, regsl[i])){
+              strcpy(tokens[nr_token].str, itoa(cpu.gpr[i]._32)); break;
+            }
+          }
+          break;
+        default: break;
+        }
+        tokens[nr_token++].type = rules[i].token_type;
 
         break;
       }
@@ -109,6 +127,11 @@ static bool make_token(char *e) {
   return true;
 }
 
+uint32_t eval(uint32_t p, uint32_t q);
+bool check_parentheses(uint32_t p, uint32_t q);
+Token* get_main_op(uint32_t p, uint32_t q);
+bool is_op(int type);
+
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
@@ -116,7 +139,77 @@ uint32_t expr(char *e, bool *success) {
   }
 
   /* TODO: Insert codes to evaluate the expression. */
-  // TODO();
   *success = true;
-  return 0;
+  return eval(0, nr_token);
+}
+
+uint32_t eval(uint32_t p, uint32_t q) {
+  if (p > q) {
+    /* Bad expression */
+    panic("bad experssion");
+  }
+  else if (p == q) {
+    /* Single token.
+     * For now this token should be a number.
+     * Return the value of the number.
+     */
+    return atoi(tokens[p].str);
+  }
+  else if (check_parentheses(p, q) == true) {
+    /* The expression is surrounded by a matched pair of parentheses.
+     * If that is the case, just throw away the parentheses.
+     */
+    return eval(p + 1, q - 1);
+  }
+  else {
+    /* We should do more things here. */
+    // the position of main op in the token expression
+    Token* op = get_main_op(p, q);
+    uint32_t val1 = eval(p, op - 1);
+    uint32_t val2 = eval(op + 1, q);
+
+    switch (op->type) {
+      case '+': return val1 + val2; break;
+      case '-': return val1 - val2; break;
+      case '*': return val1 * val2; break;
+      case '/': return val1 / val2; break;
+      default: assert(0);
+    }
+  }
+}
+
+bool check_parentheses(uint32_t p, uint32_t q){
+  if(tokens[p].type != '(' || tokens[q].type != ')'){
+    return false;
+  }
+  int idx = 0;
+  for(int i=p;i<=q;i++){
+    if(tokens[i].type == '('){
+      idx++;
+    } else if(tokens[i].type == ')'){
+      idx--;
+      if(idx == 0) return false;
+    }
+  }
+  if(idx > 0) return false;
+  return true;
+}
+
+Token* get_main_op(uint32_t p, uint32_t q){
+  int p_idx = 0, t_idx = -1;
+  for(int i=q;i>=p;i--){
+    if(tokens[i].type == ')'){
+      p_idx++;
+    } else if(tokens[i].type == '('){
+      p_idx--;
+    } else if(p_idx == 0 && is_op(tokens[i].type)){
+      t_idx = i;
+    }
+  }
+  assert(t_idx > 0);
+  return tokens + t_idx;
+}
+
+inline bool is_op(int type){
+  return type == '+' || type == '-' || type == '*' || type == '/';
 }
