36ca9c310d524f019589e85fe7d5c3bb0edb3e9c
tracer-ics2019 <tracer@njuics.org>
2022-08-24 12:11:21 +0800

    >  compile 201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:11:21 up 18 min,  0 users,  load average: 0.07, 0.04, 0.00 bc19fd72e20bf956f59c9889c0f44727acfd8c84

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -1,29 +1,29 @@
 #include "nemu.h"
 
 const char *regsl[] = {
-  "0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
+  "$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2",
   "s0", "s1", "a0", "a1", "a2", "a3", "a4", "a5",
   "a6", "a7", "s2", "s3", "s4", "s5", "s6", "s7",
   "s8", "s9", "s10", "s11", "t3", "t4", "t5", "t6"
 };
 
 void isa_reg_display() {
-  Log("%s\t%x%d\n", "pc", cpu.pc, cpu.pc);
+  Log("%s\t%x%d", "pc", cpu.pc, cpu.pc);
   for(int i=0;i<32;i++){
-    Log("%s\t%x\t%d\n", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
+    Log("%s\t%x\t%d", regsl[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
   }
 }
 
 uint32_t isa_reg_str2val(const char *s, bool *success) {
-  *success = true;
-  if(!strcmp(s, "$pc")){
-    return cpu.pc;
-  }
-  for(int i=0;i<32;i++){
-    if(!strcmp(s+1, regsl[i])){
-      return cpu.gpr[i]._32;
-    }
-  }
-  *success = false;
-  return 0;
+//   *success = true;
+//   if(!strcmp(s, "$pc")){
+//     return cpu.pc;
+//   }
+//   for(int i=0;i<32;i++){
+//     if(!strcmp(s+1, regsl[i])){
+//       return cpu.gpr[i]._32;
+//     }
+//   }
+//   *success = false;
+//   return 0;
 }
