4ba21ef8be9e1e35af87edb660c152401d735e70
tracer-ics2019 <tracer@njuics.org>
2022-08-25 03:48:26 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:48:26 up 10:21,  0 users,  load average: 0.04, 0.05, 0.02 7ee19d77c76bab67c9e99f2b95f5cb41ca1db09f

--- nemu/src/isa/riscv32/decode.c
+++ nemu/src/isa/riscv32/decode.c
@@ -58,8 +58,8 @@ make_DHelper(I) {
   decode_op_i(id_src2, decinfo.isa.instr.simm11_0, true);
   decode_op_r(id_dest, decinfo.isa.instr.rd, true);
 
-  print_Dop(id_src->str, OP_STR_SIZE, "%d", reg_name(id_src->reg, 4));
-  print_Dop(id_src2->str, OP_STR_SIZE, "%d", id_src2->val);
+  print_Dop(id_src->str, OP_STR_SIZE, "%s", reg_name(id_src->reg, 4));
+  print_Dop(id_src2->str, OP_STR_SIZE, "%x", id_src2->val);
   print_Dop(id_dest->str, OP_STR_SIZE, "%s", reg_name(id_dest->reg, 4));
 }
 
