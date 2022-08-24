8e7d43eb34bfea27a08b02db4c56ecfb4b04bf38
tracer-ics2019 <tracer@njuics.org>
2022-08-25 03:59:13 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:59:13 up 10:32,  0 users,  load average: 0.14, 0.10, 0.05 ace5b7dd12d945d9137a9cc724c14944f1363ff5

--- nemu/src/isa/riscv32/exec/compute.c
+++ nemu/src/isa/riscv32/exec/compute.c
@@ -7,7 +7,8 @@ make_EHelper(lui) {
 }
 
 make_EHelper(addi) {
-  rtl_addi(&id_dest->reg, &id_src->reg, id_src2->val);
+  rtl_addi(&id_dest->val, &id_src->val, id_src2->val);
+  rtl_sr(id_dest->reg, &id_dest->val, 4);
 
   print_asm_template3(addi);
 }
