15a705310d786086cafcb8e0646abffcb8531b46
tracer-ics2019 <tracer@njuics.org>
2022-08-27 02:55:23 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:55:23 up 15:06,  0 users,  load average: 0.18, 0.09, 0.10 666e71fc1210bc05a14107b0e8b622225121255a

--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,82 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  return 0;
+  char* p = s;
+  size_t len = 0;
+  while(p != '\0') p++, len++;
+  return len;
 }
 
 char *strcpy(char* dst,const char* src) {
-  return NULL;
+  char* p = src;
+  while(p != '\0'){
+    *dst = *p++;
+  }
+  *dst = *p;
+  return dst;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
-  return NULL;
+  char* p = src;
+  while(p != '\0' && n > 0){
+    *dst = *p++; n--;
+  }
+  if(n > 0) *dst = *p;
+  return dst;
 }
 
 char* strcat(char* dst, const char* src) {
-  return NULL;
+  char *p = dst, *q = src;
+  while(p != '\0') p++;
+  while(q != '\0'){
+    *p = *q; p++; q++;
+  }
+  *p = *q;
+  return dst;
 }
 
 int strcmp(const char* s1, const char* s2) {
-  return 0;
+  char *p = s1, *q = s2;
+  while(p != '\0' && q != '\0' && p == q) s1++, s2++;
+  if(p == '\0' && q == '\0') return 0;
+  if(p == '\0') return -1;
+  if(q == '\0') return 1;
+  return *p < *q ? -1 : 1;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
-  return 0;
+  char *p = s1, *q = s2;
+  while(p != '\0' && q != '\0' && p == q && n > 0) s1++, s2++, n--;
+  if(n > 0) return *p < *q ? -1 : 1;
+  if(p == '\0' && q == '\0') return 0;
+  if(p == '\0') return -1;
+  if(q == '\0') return 1;
+  return *p < *q ? -1 : 1;
 }
 
 void* memset(void* v,int c,size_t n) {
-  return NULL;
+  char* a = v;
+  for(size_t i=0;i<n;i++){
+    a[i] = c;
+  }
+  return c;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
-  return NULL;
+  char *p = out, *q = in;
+  while(n--){
+    *p = *q; p++; q++;
+  }
+  return out;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+  char *p = s1, *q = s2;
+  while(n--){
+    if(*p > *q) return 1;
+    if(*p < *q) return -1;
+    p++, q++;
+  }
   return 0;
 }
 
