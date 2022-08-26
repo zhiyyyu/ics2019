a4ad485d7869fcefa14eec13252d49e24cf92003
tracer-ics2019 <tracer@njuics.org>
2022-08-27 00:04:22 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  00:04:22 up 12:18,  0 users,  load average: 0.53, 0.54, 0.29 7013be175b014ec51d124ba378ce3a5799262586

--- nemu/src/isa/riscv32/diff-test.c
+++ nemu/src/isa/riscv32/diff-test.c
@@ -10,7 +10,8 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
   for(int i=0;i<32;i++){
     if(ref_r->gpr[i]._32 != cpu.gpr[i]._32){
-      Log("%s reg differs", regsl[i]);
+      Log("%s reg differs: %d %d", 
+        regsl[i], ref_r->gpr[i]._32, cpu.gpr[i]._32);
       return false;
     }
   }
