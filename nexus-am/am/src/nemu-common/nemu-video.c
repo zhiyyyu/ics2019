c805f5646a8492b3adb13c7dc4f27b5ab8e1107b
tracer-ics2019 <tracer@njuics.org>
2022-08-28 03:27:52 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:27:52 up 11:16,  0 users,  load average: 0.16, 0.13, 0.17 c27ed01a536ab751f8fae32b687f63d9ecf8e419

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,19 +2,23 @@
 #include <amdev.h>
 #include <nemu.h>
 
+static int W;
+static int H;
+static uint32_t* fb;
+
+#define min(x, y) (x < y ? x : y)
+
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       uint32_t wh = inl(SCREEN_ADDR);
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = wh >> 16;
-      info->height = wh & 0xffff;
+      W = wh >> 16;
+      H = wh & 0xffff;
+      info->width = W;
+      info->height = H;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
-    // case _DEVREG_VIDEO_FBCTL: {
-    //   _DEV_VIDEO_FBCTL_t *info = (_DEV_VIDEO_FBCTL_t *)buf;
-    //   info->x = 
-    // }
   }
   return 0;
 }
@@ -23,7 +27,13 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int cp_bytes = sizeof(uint32_t) * min(w, W - x);
+      for (int j = 0; j < h && y + j < H; j ++) {
+        memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        pixels += w;
+      }
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
@@ -34,9 +44,8 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
-  int i;
+  fb = (uint32_t *)(uintptr_t)FB_ADDR;
   int size = screen_width() * screen_height();
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  for (i = 0; i < size; i ++) fb[i] = i;
+  for (int i = 0; i < size; i ++) fb[i] = i;
   draw_sync();
 }
