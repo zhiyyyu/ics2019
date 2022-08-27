c7336c03f7f34a6a42b499653698033481988047
tracer-ics2019 <tracer@njuics.org>
2022-08-27 15:20:13 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  15:20:13 up  2:38,  0 users,  load average: 0.13, 0.10, 0.08 72cdfe09e183da1469977fdbb11746b7a22d78fc

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -13,16 +13,41 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  char *p = fmt, *q = out, *s;
+  int num = 0;
+  while(*p != '\0'){
+    if(*p++ != '%') continue;
+    switch (*p){
+    case 'd':
+      num = va_arg(ap, int);
+      if(num < 0){
+        *q++ = '-'; num = -num;
+      }
+      s = atoi(num);
+      strcpy(q, s);
+      q += strlen(s);
+      break;
+    case 's':
+      s = va_arg(ap, char*);
+      strcpy(q, s);
+      q += strlen(s);
+      break;
+    default:
+      *q++ = *p;
+      break;
+    }
+    p++;
+  }
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  // va_list ap;
-  // int ret = -1;
-  // va_start(ap, format);
-  // ret = vsprintf(str, format, ap);
-  // va_end(ap);
-  // return ret;
+  va_list args;
+  int ret = -1;
+  va_start(args, fmt);  // 变参放入堆栈构造
+  ret = vsprintf(out, fmt, args);
+  va_end(args);
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
