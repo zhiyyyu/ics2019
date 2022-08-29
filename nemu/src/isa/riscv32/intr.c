f4fe64773c3319e290053fac29c21253c6eafa91
tracer-ics2019 <tracer@njuics.org>
2022-08-29 16:26:10 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  16:26:10 up  5:53,  0 users,  load average: 0.11, 0.15, 0.11 f41b083daaacca2230a57c528d42357adfaa2ad0

--- nemu/src/isa/riscv32/intr.c
+++ nemu/src/isa/riscv32/intr.c
@@ -7,14 +7,14 @@ void raise_intr(uint32_t NO, vaddr_t epc) {
   rtl_li(&sepc, epc);
   rtl_li(&scause, NO);
   rtl_mv(&s0, &stvec);
-  Log("ecall jump to %x", s0);
+  // Log("ecall jump to %x", s0);
   // Log("scause: %d, sstatus %d, sepc 0X%x\n", scause, sstatus, sepc);
   rtl_j(s0);
 }
 
 void return_intr() {
   rtl_mv(&s0, &sepc);
-  rtl_j(s0);
+  rtl_j(s0 + 4);
 }
 
 bool isa_query_intr(void) {
