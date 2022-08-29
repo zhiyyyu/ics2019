47fae49fd947e718369e87f4bee8688895fb7675
tracer-ics2019 <tracer@njuics.org>
2022-08-29 10:25:32 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  10:25:32 up 46 min,  0 users,  load average: 0.22, 0.12, 0.10 4ae088b94c9542fbc69f146bcc7f2461e50b8c03

--- nexus-am/am/src/riscv32/nemu/cte.c
+++ nexus-am/am/src/riscv32/nemu/cte.c
@@ -5,7 +5,9 @@
 static _Context* (*user_handler)(_Event, _Context*) = NULL;
 
 _Context* __am_irq_handle(_Context *c) {
-  printf("scause: %d, sstatus %d, sepc 0X%x", c->cause, c->status, c->epc);
+  printf("__am_irq_handle: ");
+  printf("scause: %d, sstatus %d, sepc 0X%x\n", c->cause, c->status, c->epc);
+  
   _Context *next = c;
   if (user_handler) {
     _Event ev = {0};
