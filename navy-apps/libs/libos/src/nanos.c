453ff3e3b55c9a2566d565a539496bfd20cdbd8b
tracer-ics2019 <tracer@njuics.org>
2022-08-30 17:49:27 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:27 up  2:55,  0 users,  load average: 0.22, 0.13, 0.14 4a870d145a823b1de095714f1d75939149f6f535

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -49,7 +49,7 @@ intptr_t _syscall_(intptr_t type, intptr_t a0, intptr_t a1, intptr_t a2) {
 }
 
 void _exit(int status) {
-  _syscall_(status, status, 0, 0);
+  _syscall_(SYS_exit, status, 0, 0);
   while (1);
 }
 
@@ -59,7 +59,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count) {
-  _exit(SYS_write);
+  _syscall_(SYS_write, fd, buf, count);
   return 0;
 }
 
