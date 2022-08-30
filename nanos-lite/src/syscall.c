e7633a5bc6263cbf9b0fc42874d6e422288a445d
tracer-ics2019 <tracer@njuics.org>
2022-08-31 02:36:41 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  02:36:41 up  9:48,  0 users,  load average: 0.42, 0.21, 0.12 8ee801bdd921a3028aa50acc99baa129a05ffe82

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -20,7 +20,8 @@ _Context* do_syscall(_Context *c) {
     _yield(); return NULL;
   case SYS_write: 
     Log("sys_write");
-    c->GPRx = fs_write(a[1], (void*) a[2], a[3]); return NULL;
+    // c->GPRx = fs_write(a[1], (void*) a[2], a[3]); 
+    return NULL;
   case SYS_brk:
     Log("sys_brk");
     return NULL;
