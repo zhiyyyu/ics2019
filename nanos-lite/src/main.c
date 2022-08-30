ff71eee40822e4eb4eec12561c49d0b69979d282
tracer-ics2019 <tracer@njuics.org>
2022-08-30 17:21:37 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:21:37 up  2:27,  0 users,  load average: 0.20, 0.23, 0.32 4a3d60cb66783ff9fcf56c47ed0014b3204f31ea

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -14,6 +14,10 @@ int main() {
   Log("'Hello World!' from Nanos-lite");
   Log("Build time: %s, %s", __TIME__, __DATE__);
 
+#ifdef DIFF_TEST
+  difftest_dettach();
+#endif
+
 #ifdef HAS_VME
   init_mm();
 #endif
