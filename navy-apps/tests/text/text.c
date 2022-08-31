a9433c6b86ebaf72f23f012d996b88e93499c633
tracer-ics2019 <tracer@njuics.org>
2022-08-31 17:47:50 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:47:50 up  4:15,  0 users,  load average: 0.45, 0.35, 0.18 9dc8e5eab900feb4efd5423b32fc74b4cf9ea21e

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -13,7 +13,7 @@ int main() {
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
-    printf("%d %d\n", i, n);
+    printf("1: %d %d\n", i, n);
     assert(n == i + 1);
   }
 
@@ -24,12 +24,14 @@ int main() {
 
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
+    printf("2: %d %d\n", i, n);
     assert(n == i + 1);
   }
 
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fscanf(fp, "%d", &n);
+    printf("3: %d %d\n", i, n);
     assert(n == i + 1 + 1000);
   }
 
