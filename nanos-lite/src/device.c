2b101e3e86f7cae5af1f2332f93ed9c6e71471ca
tracer-ics2019 <tracer@njuics.org>
2022-08-31 22:30:24 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:30:24 up 24 min,  0 users,  load average: 0.82, 0.64, 0.65 e541b02db4c636bec09193a1ea670d4c1040f2f7

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,7 +19,7 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
   int key = read_key();
   int time = uptime();
-  if(key & 0x7fff == _KEY_NONE){
+  if((key & 0x7fff) == _KEY_NONE){
     Log("time");
     len = snprintf(buf, len, "t %d\n", time);
   } else if(key & 0x8000){
