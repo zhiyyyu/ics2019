9a605d9fcd6f5142640db77543e77ba73a8455c0
tracer-ics2019 <tracer@njuics.org>
2022-09-01 01:40:34 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  01:40:34 up 38 min,  0 users,  load average: 0.34, 0.28, 0.19 d57b1d143f5b89dec38e7a1ca91172dd25addfdc

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -37,8 +37,9 @@ static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
   // Log("dispinfo_read");
-  strncpy(buf, dispinfo + offset, len);
-  Log("%d %s %s %d", len, buf, dispinfo, offset);
+  len = strlen(dispinfo) - offset;
+  strncpy((char*) buf, dispinfo + offset, len);
+  // Log("%d %s %s %d", len, buf, dispinfo, offset);
   return len;
 }
 
