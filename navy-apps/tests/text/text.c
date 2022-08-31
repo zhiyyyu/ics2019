1a64364862f54b90bf666338f735200391106c42
tracer-ics2019 <tracer@njuics.org>
2022-08-31 17:49:09 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:49:09 up  4:17,  0 users,  load average: 0.69, 0.43, 0.22 72a26c5883fc53329e02926eb41da2be981be47

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -20,6 +20,7 @@ int main() {
   fseek(fp, 0, SEEK_SET);
   for (i = 0; i < 500; i ++) {
     fprintf(fp, "%4d\n", i + 1 + 1000);
+    printf("%4d\n", i+1+1000);
   }
 
   for (i = 500; i < 1000; i ++) {
