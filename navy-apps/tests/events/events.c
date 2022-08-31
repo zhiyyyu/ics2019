2c49a8f87ff4711f598bd3018fd1a35ba2742b9c
tracer-ics2019 <tracer@njuics.org>
2022-08-31 22:10:42 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:10:42 up 4 min,  0 users,  load average: 1.16, 0.70, 0.31 147384ea80ce669c5b9199b8837ad37221c43fdd

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -6,11 +6,12 @@ int main() {
   printf("Start to receive events...\n");
   int i = 0;
   while (1) {
-    if(i++ > 100000) break;
+    if(i++ > 10000) break;
     char buf[256];
     char *p = buf, ch;
     while ((ch = fgetc(fp)) != -1) {
       *p ++ = ch;
+      putchar(ch);
       if(ch == '\n') {
         *p = '\0';
         break;
