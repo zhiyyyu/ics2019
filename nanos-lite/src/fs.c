4fbac33ec6e59430fb94a0d74e881a020cf5a448
tracer-ics2019 <tracer@njuics.org>
2022-08-31 17:45:31 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:45:30 up  4:13,  0 users,  load average: 0.44, 0.32, 0.14 38919b3c52923602c4be546e2af060c1fca5be9f

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -89,7 +89,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
   // 读取[open_offset, open_offset + len)
   size_t offset = file_table[fd].open_offset;
   // CHECK_OFFSET(fd, offset + len);
-  len = offset + len > file_table[fd].size ? file_table[fd].size - offset : len;
+  len = offset + len >= file_table[fd].size ? file_table[fd].size - offset : len;
   ramdisk_read(buf, offset + file_table[fd].disk_offset, len);
   file_table[fd].open_offset += len;
   return len;
