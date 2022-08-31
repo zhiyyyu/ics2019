baa3599581034289e0c3975f4c334b1169f09c7f
tracer-ics2019 <tracer@njuics.org>
2022-08-31 22:52:41 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  22:52:41 up 46 min,  0 users,  load average: 0.50, 0.43, 0.46 4906fd4a38c7186f16ff5a4cd74c41738100250a

--- navy-apps/tests/events/events.c
+++ navy-apps/tests/events/events.c
@@ -4,25 +4,18 @@ int main() {
   FILE *fp = fopen("/dev/events", "r");
   int time = 0;
   printf("Start to receive events...\n");
-  int i = 0;
   while (1) {
-    // if(i++ > 10) break;
     char buf[256];
     char *p = buf, ch;
     while ((ch = fgetc(fp)) != -1) {
       if(ch == '\0') continue;
-      *p = ch;
-      printf("%d %c\n", p-buf, *p);
-      p++;
+      *p++ = ch;
       if(ch == '\n') {
         *p = '\0';
         break;
       }
     }
-    for(int ii=0;ii<256;ii++) putchar(buf[ii]);
-    // printf("buf: %s buf[0]: %c\n", buf, buf[0]);
-    if(buf[0] == '\0') continue;
-
+    
     int is_time = buf[0] == 't';
     time += is_time;
     if (!is_time) {
