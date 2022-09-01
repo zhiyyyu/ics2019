9fb3179cfe8f2571d86dcf22a44b618c990ffdf8
tracer-ics2019 <tracer@njuics.org>
2022-09-01 13:05:14 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  13:05:14 up  1:28,  0 users,  load average: 1.43, 1.36, 1.34 b5d124e27fe35e355c58e1b3ea9afc6d3ed635d

--- navy-apps/apps/pal/src/ui/ui.c
+++ navy-apps/apps/pal/src/ui/ui.c
@@ -720,7 +720,7 @@ PAL_LoadObjectDesc(
    //
    while (fgets(buf, 512, fp) != NULL)
    {
-      printf("fgets");
+      printf("fgets buf: %s\n", buf);
       p = strchr(buf, '=');
       if (p == NULL)
       {
