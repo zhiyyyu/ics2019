966a5fbe94171f13eefdf30859f4a1afd1d06925
tracer-ics2019 <tracer@njuics.org>
2022-09-01 12:23:15 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  12:23:15 up 46 min,  0 users,  load average: 0.48, 0.25, 0.25 2bd296fb11bfeec5e814ce4f7d6ad76c2e6cf46e

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -47,7 +47,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   // Log("fb_write");
   int w = screen_width();
-  int h = screen_height();
+  // int h = screen_height();
   int x = (offset / 4) % w, y = (offset / 4) / w;
   draw_rect((uint32_t*) buf, x, y, len / 4, 1);
   return len;
