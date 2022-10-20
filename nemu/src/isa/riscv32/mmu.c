e6ccee70c8a1da099452599c69b54aa60574a8df
tracer-ics2019 <tracer@njuics.org>
2022-10-21 07:14:41 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:14:41 up  4:32,  0 users,  load average: 0.18, 0.10, 0.07 a4f6b8f62c54dc1351b9e90ddcaa610f80df6342

--- nemu/src/isa/riscv32/mmu.c
+++ nemu/src/isa/riscv32/mmu.c
@@ -15,8 +15,8 @@ paddr_t page_translate(vaddr_t addr) {
   uint32_t ppn = paddr_read((satp << 12) | (vpn_1 << 2) , 4);
   assert((ppn & 1) == 1);
   ppn &= 0x3fffff << 12;
-  Log("second pte: 0x%x", (ppn << 12) | (vpn_0 << 2));
-  paddr_t paddr = paddr_read((ppn << 12) | (vpn_0 << 2), 4);
+  Log("second pte: 0x%x", (ppn &(~0x3ff)) | (vpn_0 << 2));
+  paddr_t paddr = paddr_read((ppn &(~0x3ff)) | (vpn_0 << 2), 4);
   assert((paddr & 1) == 1);
   Log("page translate: 0x%x -> 0x%x", addr, paddr);
   return paddr;
