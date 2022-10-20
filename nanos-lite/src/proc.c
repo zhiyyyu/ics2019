666a7ac983e083717453bc848d43267d3d4bfb65
tracer-ics2019 <tracer@njuics.org>
2022-10-21 03:23:25 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:23:25 up 40 min,  0 users,  load average: 0.21, 0.07, 0.08 c7ac9fdfacd5ee0cf33849e775a1d3ade699a5f3

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -32,10 +32,10 @@ void init_proc() {
   
   naive_uload(NULL, "/bin/init");
 
-  context_kload(&pcb[0], (void*) hello_fun);
-  // context_uload(&pcb[1], "/bin/init");
-  // context_uload(&pcb[1], "/bin/hello");
-  switch_boot_pcb();
+  // context_kload(&pcb[0], (void*) hello_fun);
+  // // context_uload(&pcb[1], "/bin/init");
+  // // context_uload(&pcb[1], "/bin/hello");
+  // switch_boot_pcb();
 }
 
 /**
