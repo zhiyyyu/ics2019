6652bad78a0d0d5a13d0e0ac87039bf3cb961437
tracer-ics2019 <tracer@njuics.org>
2022-10-21 07:20:22 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:20:22 up  4:37,  0 users,  load average: 0.15, 0.13, 0.09 5fc4e68b91ef37d665407fb8373a10e195533a01

--- nemu/src/isa/riscv32/mmu.c
+++ nemu/src/isa/riscv32/mmu.c
@@ -7,7 +7,7 @@ paddr_t page_translate(vaddr_t addr) {
   uint32_t vpn_0 = (addr >> 10) & 0x3ff;
   uint32_t vpn_1 = (addr >> 20) & 0xfff;
   uintptr_t _satp = 0x822e5000;
-  // _satp = *getCSRs(0x180);
+  _satp = *getCSRs(0x180);
   // asm ("csrrs %0, satp" : "=r"(_satp) : );
   Log("satp: 0x%x", _satp);
   _satp &= 0x3fffff << 12;
