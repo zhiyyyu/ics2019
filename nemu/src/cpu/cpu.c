e3b97a72249943ab3767a8ec2327e3cda3ea3692
tracer-ics2019 <tracer@njuics.org>
2022-10-21 05:24:17 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  05:24:17 up  2:41,  0 users,  load average: 0.23, 0.12, 0.03 20c839427dd71bf175d82f29ce68767ec29403fb

--- nemu/src/cpu/cpu.c
+++ nemu/src/cpu/cpu.c
@@ -3,7 +3,7 @@
 CPU_state cpu;
 
 rtlreg_t s0, s1, t0, t1, ir;
-rtlreg_t stvec, sepc, sstatus, scause;
+rtlreg_t stvec, sepc, sstatus, scause, satp;
 
 /* shared by all helper functions */
 DecodeInfo decinfo;
