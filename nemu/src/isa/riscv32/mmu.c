6b9184a851c72b876fc4513f0285624b1f17b4e7
tracer-ics2019 <tracer@njuics.org>
2022-10-21 07:07:51 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  07:07:51 up  4:25,  0 users,  load average: 0.30, 0.17, 0.08 d71bde87aff1e33df188b06cf2b9da48cf9f1a4d

--- nemu/src/isa/riscv32/mmu.c
+++ nemu/src/isa/riscv32/mmu.c
@@ -6,9 +6,10 @@ paddr_t page_translate(vaddr_t addr) {
   uint32_t offset = addr & 0xfff;
   uint32_t vpn_0 = (addr >> 12) & 0x3ff;
   uint32_t vpn_1 = (addr >> 22) & 0x3ff;
-  uintptr_t _satp = *getCSRs(0x180);
+  uintptr_t _satp = 0x822e5000;
+  // _satp = *getCSRs(0x180);
+  // asm ("csrrs %0, satp" : "=r"(_satp) : );
   Log("satp: 0x%x", _satp);
-  // asm ("csrr t0, satp; mv %0, t0" : "=r"(satp) : );
   _satp &= 0x3fffff;
   Log("first pte: 0x%x", (_satp << 12) | (vpn_1 << 2));
   uint32_t ppn = paddr_read((satp << 12) | (vpn_1 << 2) , 4);
@@ -19,6 +20,7 @@ paddr_t page_translate(vaddr_t addr) {
   assert((paddr & 1) == 1);
   Log("page translate: 0x%x -> 0x%x", addr, paddr);
   return paddr;
+  // return addr;
 }
 
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
