5162546f22dbd4dd95c8a5c7d270f0f007ba95cb
tracer-ics2019 <tracer@njuics.org>
2022-10-21 03:55:56 +0800

    >  run U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  03:55:56 up  1:13,  0 users,  load average: 0.08, 0.05, 0.01 85767d9b6b46fa0b64cca1e6d90935fa6c951b73

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -9,7 +9,7 @@ static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
   case _EVENT_YIELD: 
     // Log("get yield."); 
-    // return schedule(c);
+    return schedule(c);
     break;
   case _EVENT_SYSCALL: 
     // Log("get syscall.");
--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -26,10 +26,10 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   
-  // context_kload(&pcb[0], (void*) hello_fun);
+  context_kload(&pcb[0], (void*) hello_fun);
   // context_uload(&pcb[1], "/bin/init");
   // context_uload(&pcb[1], "/bin/hello");
-  // switch_boot_pcb();
+  switch_boot_pcb();
   Log("end of proc");
 
   Log("Initializing processes...");
