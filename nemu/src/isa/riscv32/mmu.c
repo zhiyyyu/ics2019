a32c5f9b1566166bb19ff3ef0004e453f57d53aa
tracer-ics2019 <tracer@njuics.org>
2022-10-21 17:06:28 +0800

    >  compile U201914922 zhiyu Linux DESKTOP-GS8T6SP 5.10.102.1-microsoft-standard-WSL2 #1 SMP Wed Mar 2 00:30:59 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  17:06:28 up 14:23,  0 users,  load average: 0.13, 0.12, 0.19 9f9d21b09d15d466da9f975bc0863d198af97df0

--- nemu/src/isa/riscv32/mmu.c
+++ nemu/src/isa/riscv32/mmu.c
@@ -3,23 +3,23 @@
 #include "cpu/exec.h"
 
 paddr_t page_translate(vaddr_t addr) {
-  uint32_t offset = addr & 0xfff;
-  uint32_t vpn_0 = (addr >> 12) & 0x3ff;
-  uint32_t vpn_1 = (addr >> 22) & 0x3ff;
-  rtlreg_t _satp = satp;
-  // asm ("csrrs %0, satp" : "=r"(_satp) : );
-  Log("satp: 0x%x", _satp);
-  _satp &= 0x3fffff;
-  Log("first pte: 0x%x", (_satp << 12) | (vpn_1 << 2));
-  paddr_t ppn = paddr_read((satp << 12) | (vpn_1 << 2) , 4);
-  assert((ppn & 1) == 1);
-  ppn >>= 10;
-  Log("second pte: 0x%x", (ppn << 12) | (vpn_0 << 2));
-  paddr_t paddr = paddr_read((ppn << 12) | (vpn_0 << 2), 4);
-  assert((paddr & 1) == 1);
-  Log("page translate: 0x%x -> 0x%x", addr, paddr);
-  return (paddr >> 12 << 12) | offset;
-  // return addr;
+  // uint32_t offset = addr & 0xfff;
+  // uint32_t vpn_0 = (addr >> 12) & 0x3ff;
+  // uint32_t vpn_1 = (addr >> 22) & 0x3ff;
+  // rtlreg_t _satp = satp;
+  // // asm ("csrrs %0, satp" : "=r"(_satp) : );
+  // Log("satp: 0x%x", _satp);
+  // _satp &= 0x3fffff;
+  // Log("first pte: 0x%x", (_satp << 12) | (vpn_1 << 2));
+  // paddr_t ppn = paddr_read((satp << 12) | (vpn_1 << 2) , 4);
+  // assert((ppn & 1) == 1);
+  // ppn >>= 10;
+  // Log("second pte: 0x%x", (ppn << 12) | (vpn_0 << 2));
+  // paddr_t paddr = paddr_read((ppn << 12) | (vpn_0 << 2), 4);
+  // assert((paddr & 1) == 1);
+  // Log("page translate: 0x%x -> 0x%x", addr, paddr);
+  // return (paddr >> 12 << 12) | offset;
+  return addr;
 }
 
 uint32_t isa_vaddr_read(vaddr_t addr, int len) {
